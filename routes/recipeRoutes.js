81685adc7736c35b72c1404ea061ee093bb00789
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2022-04-22 01:36:34 +0200

    make better

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -7,13 +7,9 @@ const router = express.Router();
 
 
 router.post('/create',recipeController.recipe_create_post);
-
 router.get('/create', recipeController.recipe_create_get);
-  
 router.get('/recipe/:id', recipeController.recipe_details);
-  
 router.delete('/recipe/:id', recipeController.recipe_delete);
-
 router.get('/recipes', recipeController.recipe_index);
   
   
